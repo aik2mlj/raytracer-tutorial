bb52df7de6e4c45bc06e84d249e9926b87fa79f3
AIK2 <aik2mlj@sjtu.edu.cn>
2021-07-20 10:23:06 +0800

    support multiple-thread rendering

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -2,21 +2,65 @@
 mod vec3;
 use image::{ImageBuffer, RgbImage};
 use indicatif::ProgressBar;
+use std::sync::mpsc::channel;
+use std::sync::Arc;
+use threadpool::ThreadPool;
 
 pub use vec3::Vec3;
 
+struct World {
+    pub height: u32,
+}
+
+impl World {
+    pub fn color(&self, _: u32, y: u32) -> u8 {
+        (y * 256 / self.height) as u8
+    }
+}
+
 fn main() {
-    let x = Vec3::new(1.0, 1.0, 1.0);
-    println!("{:?}", x);
+    let height = 512;
+    let width = 1024;
 
-    let mut img: RgbImage = ImageBuffer::new(1024, 512);
-    let bar = ProgressBar::new(1024);
+    let (tx, rx) = channel();
+    let n_jobs: usize = 32;
+    let n_workers = 4;
+    let pool = ThreadPool::new(n_workers);
 
-    for x in 0..1024 {
-        for y in 0..512 {
-            let pixel = img.get_pixel_mut(x, y);
-            let color = (x / 4) as u8;
-            *pixel = image::Rgb([color, color, color]);
+    let bar = ProgressBar::new(n_jobs as u64);
+
+    let world = Arc::new(World { height });
+
+    for i in 0..n_jobs {
+        let tx = tx.clone();
+        let world_ptr = world.clone();
+        pool.execute(move || {
+            let row_begin = height as usize * i / n_jobs;
+            let row_end = height as usize * (i + 1) / n_jobs;
+            let render_height = row_end - row_begin;
+            let mut img: RgbImage = ImageBuffer::new(width, render_height as u32);
+            for x in 0..width {
+                for (img_y, y) in (row_begin..row_end).enumerate() {
+                    let y = y as u32;
+                    let pixel = img.get_pixel_mut(x, img_y as u32);
+                    let color = world_ptr.color(x, y);
+                    *pixel = image::Rgb([color, color, color]);
+                }
+            }
+            tx.send((row_begin..row_end, img))
+                .expect("failed to send result");
+        });
+    }
+
+    let mut img: RgbImage = ImageBuffer::new(width, height);
+
+    for (rows, data) in rx.iter().take(n_jobs) {
+        for (idx, row) in rows.enumerate() {
+            for col in 0..width {
+                let row = row as u32;
+                let idx = idx as u32;
+                *img.get_pixel_mut(col, row) = *data.get_pixel(col, idx);
+            }
         }
         bar.inc(1);
     }
